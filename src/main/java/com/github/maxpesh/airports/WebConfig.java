e48b6e964b976fae39ae80a8c5ca7a4ecaee8cc9
Max Peshkov <maxpeshkovit@gmail.com>
2025-10-02 20:26:50 +0400

    Add initial support for conditional requests
    NOTE: The server-side logic for generating ETags and checking modification
    dates is not yet implemented. This is a prerequisite for returning a
    `304 Not Modified` response.
    The lastModified, eTag logic on the server side is not yet implemented.

--- src/main/java/com/github/maxpesh/airports/WebConfig.java
+++ src/main/java/com/github/maxpesh/airports/WebConfig.java
@@ -2,37 +2,75 @@ package com.github.maxpesh.airports;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.function.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 class WebConfig {
+    private Instant lastModified = Instant.now();
+    private String eTag = "deadbeef";
+
     @Bean
     RouterFunction<ServerResponse> router(Repository repo) {
         return RouterFunctions.route()
-                .path("/airports", builder -> builder
-                        .GET("/lookup", request -> {
-                            if (requestIsMalformed(request)) {
-                                return ServerResponse
-                                        .badRequest()
-                                        .body("Malformed request syntax. Expects: airports/lookup?airport=<string>&matches=<positive int>");
-                            }
-                            var airport = request.param("airport").get();
-                            var matches = Integer.parseInt(request.param("matches").get());
-                            return ServerResponse
-                                    .ok()
-                                    .body(repo.getAirportsLike(airport, matches));
-                        })
-                )
+                .filter(this::ifNoneMatch)
+                .filter(this::ifNotModifiedSince)
+                .GET("/airports/lookup", request -> {
+                    if (requestIsMalformed(request)) {
+                        return ServerResponse
+                                .badRequest()
+                                .headers(this::setCommonHeaders)
+                                .body("Malformed request syntax. " +
+                                        "Expects: airports/lookup?airport=<string>&matches=<positive int>");
+                    }
+                    String airport = request.param("airport").get();
+                    int matches = Integer.parseInt(request.param("matches").get());
+                    return ServerResponse.ok()
+                            .headers(this::setCommonHeaders)
+                            .body(repo.getAirportsLike(airport, matches));
+                })
                 .build();
     }
 
-    private boolean requestIsMalformed(ServerRequest request) {
-        var airportOpt = request.param("airport");
-        var matchesOpt = request.param("matches");
+    private ServerResponse ifNoneMatch(ServerRequest request, HandlerFunction<ServerResponse> next) throws Exception {
+        List<String> values = request.headers().header(HttpHeaders.IF_NONE_MATCH);
+        if (values.isEmpty()) {
+            return next.handle(request);
+        }
+        String eTag = values.get(0);
+        if (eTag.equals(this.eTag)) {
+            return ServerResponse.status(HttpStatus.NOT_MODIFIED)
+                    .headers(this::setCommonHeaders)
+                    .build();
+        } else {
+            return next.handle(request);
+        }
+    }
 
+    private ServerResponse ifNotModifiedSince(ServerRequest request, HandlerFunction<ServerResponse> next) throws Exception {
+        List<String> values = request.headers().header(HttpHeaders.IF_MODIFIED_SINCE);
+        if (values.isEmpty()) {
+            return next.handle(request);
+        }
+        String ifModifiedSince = values.get(0);
+        if (Instant.parse(ifModifiedSince).equals(lastModified)) {
+            return ServerResponse.status(HttpStatus.NOT_MODIFIED)
+                    .headers(this::setCommonHeaders)
+                    .build();
+        } else {
+            return next.handle(request);
+        }
+    }
+
+    private static boolean requestIsMalformed(ServerRequest request) {
+        Optional<String> airportOpt = request.param("airport");
+        Optional<String> matchesOpt = request.param("matches");
         try {
             if (airportOpt.isEmpty() || matchesOpt.isEmpty() || Integer.parseInt(matchesOpt.get()) < 1) {
                 return true;
@@ -41,5 +79,11 @@ class WebConfig {
             return true;
         }
         return false;
+    }
+
+    private void setCommonHeaders(HttpHeaders headers) {
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setLastModified(lastModified);
+        headers.setETag(eTag);
     }
 }
