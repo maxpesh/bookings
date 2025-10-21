82a546b38cb261d111677e73d8107406ff1e0795
Max Peshkov <maxpeshkovit@gmail.com>
2025-10-21 21:02:48 +0400

    Add Basic HTTP authentication with test user for private/ endpoints

--- src/main/java/com/github/maxpesh/airports/WebConfig.java
+++ src/main/java/com/github/maxpesh/airports/WebConfig.java
@@ -14,6 +14,21 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.context.NullSecurityContextRepository;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -26,10 +41,13 @@ import org.springframework.web.servlet.function.ServerResponse;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 class WebConfig {
@@ -52,6 +70,53 @@ class WebConfig {
                 .addModule(new JavaTimeModule())
                 .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS).build();
         return new MappingJackson2HttpMessageConverter(objMapper);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authConf -> {
+                    authConf.requestMatchers("*/airports/lookup/v1").permitAll();
+                    authConf.requestMatchers("/error").permitAll();
+                    authConf.anyRequest().authenticated();
+                })
+                .httpBasic(basicConf -> basicConf
+                        .realmName("private realm")
+                        .securityContextRepository(new NullSecurityContextRepository()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authenticationManager(authenticationManager());
+        return http.build();
+    }
+
+    private AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authProvider);
+    }
+
+    public UserDetailsService userDetailsService() {
+        UserDetails testUser = User.builder()
+                .passwordEncoder(s -> passwordEncoder().encode(s))
+                .username("test")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(testUser);
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return true;
+            }
+        };
     }
 
     private static boolean supportLanguage(ServerRequest request) {
